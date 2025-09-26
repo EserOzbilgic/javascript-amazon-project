12db1d06652d5260a427d9b702ca56320a14ea63
EserÖzbilgiç <eser.ozbilgic@gmail.com>
2025-09-26 14:20:56 +0300

    Create automated tests for addToCart

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,9 @@
-export let cart = JSON.parse(localStorage.getItem('cart'));
+export let cart;
+
+LoadFromStorage();
+
+export function LoadFromStorage() {
+cart = JSON.parse(localStorage.getItem('cart'));
 
 if (!cart) {
   cart = [
@@ -13,6 +18,8 @@ if (!cart) {
       deliveryOptionId: '2',
     }
   ];
+}
+
 }
 
 function saveToStorage() {
